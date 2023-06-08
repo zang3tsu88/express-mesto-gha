3c54bc0db432805fd4275a724943fbc9f662a83b
zang3tsu88 <maximenya.a@gmail.com>
2023-06-08 13:31:37 +0300

    fix: changed to single quotes

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // const STATUS_CODE_MESSAGE = require("http").STATUS_CODES;
-const http2 = require("http2").constants;
-const express = require("express");
-const mongoose = require("mongoose");
-const router = require("./routes/routes");
+const http2 = require('http2').constants;
+const express = require('express');
+const mongoose = require('mongoose');
+const router = require('./routes/routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,21 +12,21 @@ const PORT = process.env.PORT || 3000;
  * Я не очень понял, о чем вы. Можно точнее?
  */
 mongoose
-  .connect("mongodb://127.0.0.1:27017/mestodb")
-  .then(() => console.log("Connected to DB"))
-  .catch((err) => console.log("Server Connection Error!!!\n", err));
+  .connect('mongodb://127.0.0.1:27017/mestodb')
+  .then(() => console.log('Connected to DB'))
+  .catch((err) => console.log('Server Connection Error!!!\n', err));
 
 app.use(express.json());
 app.use((req, res, next) => {
   req.user = {
-    _id: "647a3fd1d9577763a78fa33c",
+    _id: '647a3fd1d9577763a78fa33c',
   };
 
   next();
 });
 app.use(router);
-app.use("*", (req, res) => {
-  res.status(http2.HTTP_STATUS_NOT_FOUND).send({ message: "Page not Found" });
+app.use('*', (req, res) => {
+  res.status(http2.HTTP_STATUS_NOT_FOUND).send({ message: 'Page not Found' });
 });
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
--- controllers/cards.js
+++ controllers/cards.js
@@ -1,5 +1,5 @@
-const http2 = require("http2").constants;
-const Card = require("../models/Card");
+const http2 = require('http2').constants;
+const Card = require('../models/Card');
 // 500 Internal Server Error
 // 404 Not Found
 // 400 Bad Request
@@ -11,7 +11,7 @@ const getCards = (req, res) => {
     })
     .catch((err) => {
       res.status(http2.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({
-        message: "Internal Server Error.",
+        message: 'Internal Server Error.',
         err: err.message,
         stack: err.stack,
       });
@@ -27,15 +27,15 @@ const createCard = (req, res) => {
       res.status(http2.HTTP_STATUS_CREATED).send(card);
     })
     .catch((err) => {
-      if (err.name === "ValidationError") {
+      if (err.name === 'ValidationError') {
         return res.status(http2.HTTP_STATUS_BAD_REQUEST).send({
-          message: "Entered invalid data.",
+          message: 'Entered invalid data.',
           err: err.message,
           stack: err.stack,
         });
       }
       return res.status(http2.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({
-        message: "Internal Server Error.",
+        message: 'Internal Server Error.',
         err: err.message,
         stack: err.stack,
       });
@@ -55,21 +55,21 @@ const deleteCardById = (req, res) => {
     .then((card) => {
       if (!card) {
         return res.status(http2.HTTP_STATUS_NOT_FOUND).send({
-          message: "No card with such id.",
+          message: 'No card with such id.',
         });
       }
       return res.send(card);
     })
     .catch((err) => {
-      if (err.name === "CastError") {
+      if (err.name === 'CastError') {
         return res.status(http2.HTTP_STATUS_BAD_REQUEST).send({
-          message: "Entered invalid data.",
+          message: 'Entered invalid data.',
           err: err.message,
           stack: err.stack,
         });
       }
       return res.status(http2.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({
-        message: "Internal Server Error.",
+        message: 'Internal Server Error.',
         err: err.message,
         stack: err.stack,
       });
@@ -80,26 +80,26 @@ const likeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
-    { new: true }
+    { new: true },
   )
     .then((card) => {
       if (!card) {
         return res.status(http2.HTTP_STATUS_NOT_FOUND).send({
-          message: "No card with such id.",
+          message: 'No card with such id.',
         });
       }
       return res.send(card);
     })
     .catch((err) => {
-      if (err.name === "CastError") {
+      if (err.name === 'CastError') {
         return res.status(http2.HTTP_STATUS_BAD_REQUEST).send({
-          message: "Entered invalid data.",
+          message: 'Entered invalid data.',
           err: err.message,
           stack: err.stack,
         });
       }
       return res.status(http2.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({
-        message: "Internal Server Error.",
+        message: 'Internal Server Error.',
         err: err.message,
         stack: err.stack,
       });
@@ -109,26 +109,26 @@ const unlikeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } }, // добавить _id в массив, если его там нет
-    { new: true }
+    { new: true },
   )
     .then((card) => {
       if (!card) {
         return res
           .status(http2.HTTP_STATUS_NOT_FOUND)
-          .send({ message: "No card with such id." });
+          .send({ message: 'No card with such id.' });
       }
       return res.send(card);
     })
     .catch((err) => {
-      if (err.name === "CastError") {
+      if (err.name === 'CastError') {
         return res.status(http2.HTTP_STATUS_BAD_REQUEST).send({
-          message: "Entered invalid data.",
+          message: 'Entered invalid data.',
           err: err.message,
           stack: err.stack,
         });
       }
       return res.status(http2.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({
-        message: "Internal Server Error.",
+        message: 'Internal Server Error.',
         err: err.message,
         stack: err.stack,
       });
